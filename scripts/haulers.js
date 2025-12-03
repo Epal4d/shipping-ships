0afb0d1baf5e37b86e6328eb38adcb78306f2830
Evan Alford <evan.al4d@gmail.com>
2025-12-03 16:24:52 -0600

    initial commit

---
+++ scripts/haulers.js
@@ -0,0 +1,14 @@
+import { getHaulers } from "./database.js";
+
+haulers = getHaulers()
+
+export const haulerList = () => {
+    let haulerHTML = "<ul class= haulers>"
+
+    haulers = getHaulers()
+
+    for (const hauler of haulers ) {
+        haulerHTML += `<li hauler-id= "${hauler.id}">${hauler.name}</li>`
+    }
+    haulerHTML += "</ul>"
+}
